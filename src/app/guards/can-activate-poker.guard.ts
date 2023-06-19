83dc6f4b7d9c7f336bcd46f4a0ca32d79b1f75af
Julien CLAUDEL <julienclaudel51@outlook.com>
2023-06-19 11:02:15 +0200

    Prevent using invalid room name

--- src/app/guards/can-activate-poker.guard.ts
+++ src/app/guards/can-activate-poker.guard.ts
@@ -1,8 +1,10 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ROOM_NAME_REGEX } from '../shared/regex/room-name.regex';
 
 export const canActivatePoker: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  if (route.children[0]?.params['roomName']) return true;
+  const roomName: string | undefined = route.children[0]?.params['roomName'];
+  if (roomName && !!roomName.match(ROOM_NAME_REGEX)) return true;
   const router = inject(Router);
   const tree: UrlTree = router.parseUrl(`/poker/philippe-room-${Math.floor(Math.random() * 100000)}`);
   return tree;
