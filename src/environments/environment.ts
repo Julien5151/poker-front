72511f99765a43d21b2d0b7e03db48379fadce4a
Julien CLAUDEL <julienclaudel51@outlook.com>
2026-01-31 12:23:20 +0100

    update domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from './environment.interface';
 
 export const environment: Environment = {
-  wsUrl: 'poker-philippe.jcl-web.ovh',
+  wsUrl: 'jcl-web.freeboxos.fr',
   prod: true,
 };
